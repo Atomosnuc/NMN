6a142f0dfa2df6898e4e4a2bc2ae432e5184aaea
Atomos Nucleous <atomosnuc@gmail.com>
2026-05-17 00:34:11 +0300

    Adds Liquidity

--- contracts/NMN.sol
+++ contracts/NMN.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 import "./Token.sol";
 
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract NMN {
+
+  enum Coin { mirian, castar, tharni, pony, penny, brass, copper }
+
   Token public mirian;
   Token public castar;
   Token public tharni;
@@ -12,6 +17,24 @@ contract NMN {
   Token public penny;
   Token public brass;
   Token public copper;
+  Token[] public tokens;
+
+  uint256 constant PRECISION = 10**18;
+
+  struct Pool {
+    uint256 reserve0;
+    uint256 reserve1;
+    uint256 K;
+    uint256 totalShares;
+    bool exists;
+  }
+
+  mapping(Coin => mapping(Coin => Pool)) public pools;
+  mapping(Coin => mapping(Coin => mapping(address => uint256))) public userShares;
+
+  error ZeroAmount();
+  error IdenticalTokens();
+  error PoolDoesNotExist();
 
   constructor(Token[] memory _tokens) {
     mirian = _tokens[0];
@@ -21,6 +44,120 @@ contract NMN {
     penny = _tokens[4];
     brass = _tokens[5];
     copper = _tokens[6];
+
+    for (uint i = 0; i <= 6 ; i++) {
+      tokens.push(_tokens[i]);
+    }
+    pools[Coin.mirian][Coin.castar]= Pool(0 ,0, 0, 0, true);
   }
+
+  // sorting functions
+  function sortCoins( Coin _coin0, Coin _coin1)
+    public pure returns (Coin coin0, Coin coin1)
+  {
+    return (_coin0 < _coin1 ? (_coin0, _coin1) : (_coin1, _coin0));
+  }
+
+  function sortCoinsAndAmounts(
+    Coin _coin0, Coin _coin1, uint256 _coin0Amount, uint256 _coin1Amount
+  ) internal pure returns (
+    Coin coin0, Coin coin1, uint256 coin0Amount, uint256 coin1Amount
+  )
+  {
+    if (_coin0 == _coin1) revert IdenticalTokens();
+    if (_coin0 < _coin1) {
+      return (_coin0, _coin1, _coin0Amount, _coin1Amount);
+    } else {
+      return (_coin1, _coin0, _coin1Amount, _coin0Amount);
+    }
+  }
+
+  function getUserShares(Coin _coin0 , Coin _coin1, address user) 
+    public view returns(uint256)
+  {
+    (Coin coin0, Coin coin1) = sortCoins(_coin0, _coin1);
+    return (userShares[coin0][coin1][user]);
+  }
+
+  function getPoolState(Coin _coin0 , Coin _coin1) 
+    public view returns(Pool memory) 
+  {
+    (Coin coin0, Coin coin1) = sortCoins(_coin0, _coin1);
+  
+    return (pools[coin0][coin1]);
+  }
+  
+  function updatePoolBalances(
+    Coin _coin1, uint256 _coin1Amount,
+    Coin _coin2, uint256 _coin2Amount
+    ) public {
+    pools[_coin1][_coin2].reserve0 += _coin1Amount;
+    pools[_coin1][_coin2].reserve1 += _coin2Amount;
+    pools[_coin1][_coin2].K = pools[_coin1][_coin2].reserve0 * pools[_coin1][_coin2].reserve1;
+    
+  }
+
+  function addLiquidity(
+    Coin _coin0, uint256 _coin0Amount, Coin _coin1, uint256 _coin1Amount
+  ) external{
+    // sort everything
+    (
+      Coin coin0, Coin coin1, uint256 coin0Amount, uint256 coin1Amount
+    ) = sortCoinsAndAmounts(_coin0, _coin1 , _coin0Amount, _coin1Amount);
+
+    Token token0 = Token(tokens[uint256(coin0)]);
+    Token token1 = Token(tokens[uint256(coin1)]);
+
+    // Deposit Tokens
+    require(
+      token0.transferFrom(msg.sender, address(this), coin0Amount),
+      string(abi.encodePacked("failed to transfer ", token0.name()))
+    );
+    require(
+      token1.transferFrom(msg.sender, address(this), coin1Amount),
+      string(abi.encodePacked("failed to transfer ", token1.name()))
+    );
+
+    Pool storage pool = pools[coin0][coin1];
+
+    //Calculate shares to mMint
+    uint256 sharesToMint;
+    if (pool.totalShares != 0) {
+      uint256 share1 = (pool.totalShares * coin0Amount) / pool.reserve0;
+      uint256 share2 = (pool.totalShares * coin1Amount) / pool.reserve1;
+      require(
+        (share1 / 10**3) == (share2 / 10**3),
+        "must provide equal token amounts"
+      );
+      sharesToMint = share1;
+    } else {
+      sharesToMint = 100 * PRECISION;
+    }
+
+    //  Manage pool
+    updatePoolBalances(coin0, coin0Amount, coin1, coin1Amount);
+    pool.totalShares += sharesToMint;
+    userShares[coin0][coin1][msg.sender] += sharesToMint;
+
+  }
+
+  function calculateLiquidityAmount(Coin _coin0, Coin _coin1,  uint256 _coin0Amount)
+    public view returns(uint256 coin1Amount) {
+    if (_coin0Amount == 0) revert ZeroAmount();
+
+    (Coin coin0, Coin coin1) = sortCoins(_coin0,_coin1);
+    Pool storage pool = pools[coin0][coin1];
+    if (!pool.exists) revert PoolDoesNotExist();
+
+    if (_coin0 == coin0) {
+      coin1Amount = (_coin0Amount * pools[coin0][coin1].reserve1) / pools[coin0][coin1].reserve0;
+    } else {
+      coin1Amount = (_coin0Amount * pools[coin0][coin1].reserve0) / pools[coin0][coin1].reserve1;
+    }
+
+   
+
+  }
+
 
 }
--- test/NMN.js
+++ test/NMN.js
@@ -13,9 +13,12 @@ describe('NMN', () => {
   let tokenAddresses = Array()
 
   beforeEach(async () => {
+    // Set up accounts
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    liquidityProvider = accounts[1]
 
+    // Deploy Tokens
     const Token = await ethers.getContractFactory('Token')
     
     mirian = await Token.deploy('Mirian','MRN','1000000')
@@ -36,23 +39,155 @@ describe('NMN', () => {
       copper.address
     ]
 
+    // Send tokens to liquidity provider
+    let transaction = await mirian.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    
+    transaction = await castar.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await tharni.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await pony.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await penny.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await brass.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+    transaction = await copper.connect(deployer).transfer(liquidityProvider.address, tokens(100000))
+    await transaction.wait()
+
+    // Deploy NMN
     const NMN = await ethers.getContractFactory('NMN')
     nmn = await NMN.deploy(tokenAddresses)
   })
 
   describe('Deployment', () => {
-    
+    let result
     it('has an address', async () => {
       expect(nmn.address).to.not.equal(0x0)
     })
     it('tracks address of all tokens', async () => {
-      expect(await nmn.mirian()).to.equal(mirian.address)
-      expect(await nmn.castar()).to.equal(castar.address)
-      expect(await nmn.tharni()).to.equal(tharni.address)
-      expect(await nmn.pony()).to.equal(pony.address)
-      expect(await nmn.penny()).to.equal(penny.address)
-      expect(await nmn.brass()).to.equal(brass.address)
-      expect(await nmn.copper()).to.equal(copper.address)
+      for(let i = 0 ; i < 7 ; i++){
+        expect(await nmn.tokens(1)).to.equal(tokenAddresses[1])
+      }
+    })
+
+  })
+
+  describe('Adding liquidity', () => {
+    let amount, transaction, result
+    beforeEach(async () => {
+      // Deployer approves 100k from each token
+      amount = tokens(100000)
+
+      transaction = await mirian.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await castar.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await tharni.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await pony.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await penny.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await brass.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+      transaction = await copper.connect(deployer).approve(nmn.address, amount)
+      await transaction.wait()
+    })
+
+    it('updates balances', async () => {
+      // Deployer adds liquidity, 1000 mirian and 4000 castar
+      amount = 1000
+      transaction = await nmn.connect(deployer)
+      .addLiquidity(
+        0,
+        tokens(amount),
+        1,
+        tokens(amount*4)
+      )
+      await transaction.wait()
+
+      // Check NMN receives tokens
+      expect(await mirian.balanceOf(nmn.address)).to.equal(tokens(amount))
+      expect(await castar.balanceOf(nmn.address)).to.equal(tokens(amount*4))
+      result = await nmn.getPoolState(0,1)
+      expect(result.reserve0).to.equal(tokens(amount))
+      expect(result.reserve1).to.equal(tokens(amount*4))
+    })
+
+    it('handles reverse order', async () => {
+      // Deployer adds liquidity, 4000 castar and 1000 mirian
+      amount = 1000
+      transaction = await nmn.connect(deployer)
+      .addLiquidity(
+        1,
+        tokens(amount*4),
+        0,
+        tokens(amount)
+      )
+      await transaction.wait()
+
+      // Check NMN receives tokens
+      expect(await mirian.balanceOf(nmn.address)).to.equal(tokens(amount))
+      expect(await castar.balanceOf(nmn.address)).to.equal(tokens(amount*4))
+      // also check balances in reverse order
+      result = await nmn.getPoolState(1,0)
+      expect(result.reserve0).to.equal(tokens(amount))
+      expect(result.reserve1).to.equal(tokens(amount*4))
+    })
+
+    it('updates shares', async () => {
+      amount = 1000
+      transaction = await nmn.connect(deployer)
+      .addLiquidity(
+        0,
+        tokens(amount),
+        1,
+        tokens(amount*4)
+      )
+      await transaction.wait()
+      //check deployer has 100 shares
+      expect(await nmn.getUserShares(1,0, deployer.address)).to.equal(tokens(100))
+      // check pool has 100 shares
+      result = await nmn.getPoolState(1,0)
+      expect(result.totalShares).to.equal(tokens(100))
+
+      ///// LP adds more liquidity/////////////////
+      // LP approves 
+
+    })
+
+    it('calculates ratio', async () => {
+      // Deployer adds liquidity, 1000 mirian and 4000 castar
+      amount = 1000
+      transaction = await nmn.connect(deployer)
+        .addLiquidity(0,tokens(amount), 1, tokens(amount*4))
+      await transaction.wait()
+      ///// LP adds more liquidity/////////////////
+      // LP approves 500 miran and 2000 castar
+      amount = 500
+      let deposit0= tokens(amount)
+      transaction = await mirian.connect(liquidityProvider).approve(nmn.address, tokens(amount))
+      await transaction.wait()
+      transaction = await castar.connect(liquidityProvider).approve(nmn.address, tokens(amount*4))
+      await transaction.wait()
+      // Calculates the amount of castar for 500 mirian
+      let deposit1 = await nmn.calculateLiquidityAmount(0, 1, deposit0)
+
+      // LP adds liquidity
+      console.log("asdasd")
+      transaction = await nmn.connect(liquidityProvider)
+        .addLiquidity(0, deposit0, 1, deposit1)
+      await transaction.wait()
+      // check pool has 100 shares
+      result = await nmn.getPoolState(1,0)
+      expect(result.totalShares).to.equal(tokens(150))
+      //Deployer should still have 100 shares
+      expect(await nmn.getUserShares(1,0, deployer.address)).to.equal(tokens(100))
+      // Pool should have now 150 shares
+      result = await nmn.getPoolState(1,0)
+      expect(result.totalShares).to.equal(tokens(150))
     })
 
   })
